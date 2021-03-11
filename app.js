b57deac9bc5e372c711a402408afffdcd445ae1a
Hannah Kemp <hfk@mail.usf.edu>
2021-03-11 16:08:29 -0500

    querying with models

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const { db } = require('./models');
-app.use(morgan('dev'));
+const morgan = require("morgan");
+const { db, User, Page } = require("./models");
+app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-db.authenticate()
-  .then(() => {
-    console.log('connected to the database');
-  })
-  
-app.get('/', (req, res)=>{
-    res.send("Hello world");
-})
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
 
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
+const init = async () => {
+  try {
+    await db.sync({ force: true });
+    const user = await User.create({ name: "Mac", email: "mac@gmail.com" });
+    const page = await Page.create({
+      title: "Instructor",
+      slug: "blah",
+      content: "javascript",
+      status: "open",
+    });
+    const user2 = await User.create({ email: "sarah@gmail.com" });
+  } catch (error) {
+    console.error();
+  }
+};
 
 const PORT = 3000;
 
@@ -23,3 +36,4 @@ app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
 
+init();
--- models/index.js
+++ models/index.js
@@ -1,28 +1,37 @@
-const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/wikistack');
+const Sequelize = require("sequelize");
+const db = new Sequelize("postgres://localhost:5432/wikistack", {
+  logging: false,
+});
 
-const Page = db.define('page', {
-    title: {
-      type: Sequelize.STRING
-    },
-    slug: {
-      type: Sequelize.STRING
-    },
-    content: {
-      type: Sequelize.TEXT
-    },
-    status: {
-      type: Sequelize.ENUM('open', 'closed')
-    }
-  });
-  
-  const User = db.define('user', {
-    name: {
-      type: Sequelize.STRING
-    },
-    email: {
-      type: Sequelize.STRING
-    }
-  });
+const Page = db.define("page", {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  slug: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  content: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+  status: {
+    type: Sequelize.ENUM("open", "closed"),
+  },
+});
 
-  module.exports = { db, Page, User };
+const User = db.define("user", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    defaultValue: "Sarah",
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+  },
+});
+
+module.exports = { db, Page, User };
